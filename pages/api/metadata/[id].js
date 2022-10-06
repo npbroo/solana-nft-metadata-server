c0841960b8d40a0e264281c5678729a793be16f8
npbroo <npbroo.biz@outlook.com>
2022-10-05 23:15:06 -0400

    json test

--- pages/api/metadata/[id].js
+++ pages/api/metadata/[id].js
@@ -16,14 +16,16 @@ const MetadataServer = async(req, res) => {
 
   const filePath = path.resolve('.', `database/${token_id}.json`)
   const imageBuffer = fs.readFileSync(filePath)
+  res.setHeader('Content-Type', 'application/json');
   res.send(imageBuffer)
   //const example = await fs.readFile('./example.json');
   //return res.status(200).json({example});
-
+/*
   res.statusCode=200
   let j = {"name":"DeGod #1353","symbol":"DGOD","description":"A collection of 10,000 of the most degenerate gods in the universe.","seller_fee_basis_points":999,"image":"https://metadata.degods.com/g/1352-dead.png","external_url":"https://degods.com","attributes":[{"trait_type":"background","value":"Deep Ocean Blue"},{"trait_type":"skin","value":"Turquoise"},{"trait_type":"specialty","value":"None"},{"trait_type":"clothes","value":"Silk Robe"},{"trait_type":"neck","value":"None"},{"trait_type":"head","value":"Propeller Hat"},{"trait_type":"eyes","value":"Laser Eyes"},{"trait_type":"mouth","value":"None"},{"trait_type":"version","value":"DeadGod"},{"trait_type":"y00t","value":"Claimed"}],"collection":{"name":"DeGods","family":"Godplex"},"properties":{"files":[{"uri":"https://metadata.degods.com/g/1352-dead.png","type":"image/png"}],"category":"image","creators":[{"address":"AxFuniPo7RaDgPH6Gizf4GZmLQFc4M5ipckeeZfkrPNn","share":100}]}}
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json(j)
+  */
   /*
   if(parseInt(token_id) <= total_tokens && token_id > 0 ) {
 
